4d129cf1ee426f356c7835f8652c3d02fe1db72e
saicharanjith-cpu <saicharanjith@gmail.com>
2026-03-22 22:53:43 +0530

    uc5

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,16 @@
-public class OOPSBannerApp{
-    // Main method to run the banner display
-    // Print each line of the OOPS banner to create the visual effect for the // message "OOPS". Construct each line using string concatenation.
-    // Each line corresponds to a row in the banner for the letters 0, 0, P, S.
-    // Using individual print statements for each line.
-    public static void main (String[] args){
-        System.out.println("   ***   " + "    ***   " + " ******   " + "   ***** ");
-        System.out.println(" **   ** " + "  **   ** " + " **    ** " + " **      ");
-        System.out.println("**     **" + " **     **" + " **    ** " + "**       ");
-        System.out.println("**     **" + " **     **" + " ******   " + "  *****  ");
-        System.out.println("**     **" + " **     **" + " **       " + "      ** ");
-        System.out.println(" **   ** " + "  **   ** " + " **       " + " **   ** ");
-        System.out.println("   ***   " + "    ***   " + " **       " + "  *****  ");
+public class OOPS {
+    public static void main(String[] args) {
+
+        String[] oops = {
+            " ***    ***    ****    ****  ",
+            "*   *  *   *   *   *  *      ",
+            "*   *  *   *   ****    ****  ",
+            "*   *  *   *   *           *  ",
+            " ***    ***    *       ****    "
+        };
+
+        for (String line : oops) {
+            System.out.println(line);
+        }
     }
 }
