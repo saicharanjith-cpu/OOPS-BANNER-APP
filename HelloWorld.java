9118818fd1365ff6d3b07034c178b5c049322d5c
saicharanjith-cpu <saicharanjith@gmail.com>
2026-03-22 22:42:16 +0530

    Refactor OOPSBannerApp to use String arrays
    Refactor OOPSBannerApp to OOPSBannerArray using String arrays and loops for modularity.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,38 @@
-public class OOPSBannerApp{
-    // Main method to run the banner display
-    // Print each line of the OOPS banner to create the visual effect for the // message "OOPS". Construct each line using string concatenation.
-    // Each line corresponds to a row in the banner for the letters 0, 0, P, S.
-    // Using individual print statements for each line.
-    public static void main (String[] args){
-        System.out.println("   ***   " + "    ***   " + " ******   " + "   ***** ");
-        System.out.println(" **   ** " + "  **   ** " + " **    ** " + " **      ");
-        System.out.println("**     **" + " **     **" + " **    ** " + "**       ");
-        System.out.println("**     **" + " **     **" + " ******   " + "  *****  ");
-        System.out.println("**     **" + " **     **" + " **       " + "      ** ");
-        System.out.println(" **   ** " + "  **   ** " + " **       " + " **   ** ");
-        System.out.println("   ***   " + "    ***   " + " **       " + "  *****  ");
-    }
-}
+/**
+ * OOPSBannerApp UC4 - Render OOPS as Banner using String Array and Loop
+ *
+ * This use case improves upon UC3 by using a String array to store banner lines
+ * and iterating through them with a for-each loop, eliminating hardcoded print
+ * statements and improving modularity and reusability.
+ *
+ * @author Developer
+ * @version 4
+ */
+
+// Extend the user story 3 to further develop the OOPS Banner Application
+// by using String arrays to hold banner lines and printing them in a loop.
+// This use case improves upon UC3 by using a String array to store banner lines
+// and iterating through them with a for-each loop, eliminating hardcoded print
+// statements and improving modularity and reusability.
+public class OOPSBannerArray {
+     
+
+    public static void main(String[]  args){
+
+          String[] lines = new String[7];
+
+
+        lines[0] = String.join(" ", " *** ", " *** ", " **** ", " **** ");
+        lines[1] = String.join(" ", "*   *", "*   *", "*    *", "*     ");
+        lines[2] = String.join(" ", "*   *", "*   *", "*    *", "*     ");
+        lines[3] = String.join(" ", "*   *", "*   *", "****  ", " ***  ");
+        lines[4] = String.join(" ", "*   *", "*   *", "*     ", "    * ");
+        lines[5] = String.join(" ", "*   *", "*   *", "*     ", "*   * ");
+        lines[6] = String.join(" ", " *** ", " *** ", "*     ", " ***  ");
+
+        // Use for-each loop to print each line
+        for (String line : lines) {
+            System.out.println(line);
+                                 }
+                                            }
+                                }
